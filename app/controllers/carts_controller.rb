4d2ef46e80525db02d03629f231a1fed3ad87cbf
Chris Robertson <chrisxrobertson@gmail.com>
2017-05-19 13:04:57 +1000

    Add Braintree Sandbox API keys

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,4 +1,9 @@
 class CartsController < ApplicationController
+  Braintree::Configuration.environment = :sandbox
+  Braintree::Configuration.merchant_id = ENV['MERCHANT_ID']
+  Braintree::Configuration.public_key = ENV['PUBLIC_KEY']
+  Braintree::Configuration.private_key = ENV['PRIVATE_KEY']
+
   def show
     @order_items = current_order.order_items
   end
